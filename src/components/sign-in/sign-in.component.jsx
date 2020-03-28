0209f4f727237d9b8d5295307dc2b9d08510b47c
Glenn Pinto <gvpinto@gmail.com>
2020-03-28 19:52:54 -0400

    Finishing off Sign In component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle} from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle} from '../../firebase/firebase.utils';
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
@@ -15,8 +15,20 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async (event) => {
         event.preventDefault();
+
+        const {email, password} = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({
+                email: '',
+                password: ''
+            });
+        } catch (error) {
+            console.error(error);
+        }
 
         this.setState({email: '', password: ''});
     }
@@ -27,6 +39,7 @@ class SignIn extends React.Component {
     }
 
     render() {
+        
         return (
             <div className="sign-in">
                 <h2>I already have an account</h2>
